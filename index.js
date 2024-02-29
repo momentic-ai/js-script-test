5ca7f7def83b50f396340565330c0e1be8eb7fd0
wuweiweiwu <wei-wei@momentic.ai>
2024-02-29 14:26:23 -0800

    initial setup

---
+++ index.js
@@ -0,0 +1,10 @@
+import { faker } from "@faker-js/faker";
+import assert from "assert";
+import axios from "axios";
+import moment from "moment";
+
+async function main() {
+  // inject the script here that you want to run from the momentic app
+}
+
+main();
